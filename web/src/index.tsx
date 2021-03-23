6385739f335c1dadeba803f054d759b1b04796b5
DenisEps <denis.epshteyn@gmail.com>
2021-03-23 12:16:47 +0300

    add antd locale provider

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ConfigProvider } from 'antd';
+import 'antd/dist/antd.css';
+import ruRU from 'antd/lib/locale/ru_RU';
+
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider locale={ruRU}>
+      <App />
+    </ConfigProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
